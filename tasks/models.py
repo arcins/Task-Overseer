e3093f5d57a46f0dd37944059837d53d73cc1907
Marcin <mszczep1@wsisiz.edu.pl>
2019-01-10 00:55:42 +0100

    created tasks model and first test

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tasks(models.Model):
+    """
+    Tasks Model
+    Defines the attributes of a task
+    """
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField()
+    description = models.TextField()
+    date = models.DateTimeField(blank=True, null=True)
+
+    def get_done(self):
+        return self.is_done
+
+    def __repr__(self):
+        return self.title + ' is added.'
---
+++ tasks/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.utils import timezone
+from ..models import Tasks
+
+
+class TaskTest(TestCase):
+    """ Test module for Puppy model """
+
+    def setUp(self):
+        Tasks.objects.create(
+            title='task1', is_done=False, description='task1 test', date = timezone.now())
+        Tasks.objects.create(
+            title='task2', is_done=True, description='task1 test', date = timezone.now())
+
+    def test_puppy_breed(self):
+        task_1 = Tasks.objects.get(title='task1')
+        task_2 = Tasks.objects.get(title='task2')
+        self.assertEqual(
+            task_1.get_done(), False)
+        self.assertEqual(
+            task_2.get_done(), True)
